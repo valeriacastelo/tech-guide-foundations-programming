7a18eb7c4ec2b7ee1af006bdc35b1812b2acfc33
valeriacastelobranco <valeriacastelobranco@gmail.com>
2019-05-31 16:17:06 +0100

    adding StringSplosion

---
+++ src/com/test/StringSplosion.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringSplosion {
+
+	public static void main(String[] args) {
+		
+		assertEquals(stringSplosion("Code"), "CCoCodCode");
+		assertEquals(stringSplosion("abc"), "aababc");
+		assertEquals(stringSplosion("ab"), "aab");
+		
+	}
+
+	public static String stringSplosion(String str) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			sb.append(str.substring(0, i + 1));
+		}
+		return sb.toString();
+	}
+
+}
